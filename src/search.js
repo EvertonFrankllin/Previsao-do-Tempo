587b4cc1bf4bc9bff43c98e6ad0625af138c039d
EvertonFrankllin <evertonfrankllintrc@gmail.com>
2024-05-08 15:33:58 -0300

    Botão de busca criado e lógica de pesquisa alterada

--- src/search.js
+++ src/search.js
@@ -3,22 +3,50 @@ import './Search.css';
 
 function Search(props) {
 
-    function detectSearch(){
+    function detectSearch(e){
+      e.preventDefault();
     
 
-    let currentValue = document.querySelector('[name=searchInput]')
+    let currentValue = document.querySelector('[name=detectSearch]')
     .value;
-
     /*
     Fazendo minha requisição em realtime
     alert(currentValue)
     */
-}
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
+
+        fetch(url)
+
+        .then(response=> response.json())
+
+        .then(data=>{
+
+            const {main, name, sys, weather} = data;
+
+            if(sys != undefined)
+
+                console.log(sys);
+
+            if(weather != undefined)
+
+                console.log(weather[0]['description']);
+
+        })
+
+    
+
+    }
 
   return (
     <div className="Search">
         <h3>Clima e Previsão do Tempo / Cidade / Previsão do tempo 15 dias ou Real Time / </h3>
-      <input placeholder={props.placeholder} onKeyUp={detectSearch} type="text"  name='searchInput'></input>
+
+        <form onSubmit={(e)=>detectSearch(e)}>
+
+          <input placeholder={props.placeholder} type="text"  name='detectSearch'></input>
+          <input type="submit" value="Buscar!" ></input>
+
+        </form>
     </div>
   );
 
